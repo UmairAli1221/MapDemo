378dd91e1539b38f6ea45bfffa5ac37470b9640d
GH <umair.ali1221@gmail.com>
2017-02-17 10:15:45 -0800

    first phase

--- app/src/main/java/com/example/gh/mapdemo/MapsActivity.java
+++ app/src/main/java/com/example/gh/mapdemo/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.example.gh.mapdemo;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,11 +22,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        if(googleServicesAvailable()){
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        }else{
+
+        }
     }
+
+    private boolean googleServicesAvailable() {
+        GoogleApiAvailability api=GoogleApiAvailability.getInstance();
+        int isAvailable=api.isGooglePlayServicesAvailable(this);
+        if(isAvailable== ConnectionResult.SUCCESS){
+            return true;
+        }else if(api.isUserResolvableError(isAvailable)){
+            Dialog dialog=api.getErrorDialog(this,isAvailable,0);
+            dialog.show();
+        }else {
+            Toast.makeText(this,"Cannoy Connect To Play Services",Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -31,8 +54,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-    private void TestUpload(){
-        int i = 1+1;
     }
 }
