809c21419ee09e47e31262ac1688a0b15cf851d0
GH <umair.ali1221@gmail.com>
2017-03-02 05:21:22 -0800

    GeoFire

--- app/src/main/java/com/example/gh/mapdemo/GetCityAddress.java
+++ app/src/main/java/com/example/gh/mapdemo/GetCityAddress.java
@@ -1,53 +1,83 @@
 package com.example.gh.mapdemo;
 
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class GetCityAddress extends AppCompatActivity {
-
-    EditText startPoint;
-    TextView latitude,longitude;
-    Button cnvert;
-    String address;
+    Button addressButton,btnMap;
+    TextView addressTV;
+    TextView latLongTV,latitu;
+    String lt,lg;
+    DatabaseReference databaseReference;
+    FirebaseDatabase database;
+    private static final String TAG = "GeocodingLocation";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_city_address);
-        startPoint=(EditText) findViewById(R.id.satrtDate);
-        address=startPoint.getText().toString();
-        latitude=(TextView) findViewById(R.id.latitude);
-        longitude=(TextView)findViewById(R.id.longitude);
-        cnvert=(Button)findViewById(R.id.convert);
-        cnvert.setOnClickListener(new View.OnClickListener() {
+        addressTV = (TextView) findViewById(R.id.addressTV);
+        latLongTV = (TextView) findViewById(R.id.latLongTV);
+        latitu=(TextView)findViewById(R.id.latitude);
+        btnMap=(Button)findViewById(R.id.GetMAp);
+        databaseReference=FirebaseDatabase.getInstance().getReference();
+        final GeoFire geoFire=new GeoFire(databaseReference);
+        addressButton = (Button) findViewById(R.id.addressButton);
+        addressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Geocoder coder=new Geocoder(GetCityAddress.this);
-                List<Address> address1;
-                try {
-                    address1=coder.getFromLocationName(address,1);
-                    if (address1.size()==0){
-                        Toast.makeText(GetCityAddress.this,"Address is not Found",Toast.LENGTH_LONG);
-                    }else if(address1.size()>0)
-                    {
-                    Address location=address1.get(0);
-                    double lat=  location.getLatitude();
-                    double lon=  location.getLongitude();
-                    latitude.setText(String.valueOf(lat));
-                    longitude.setText(String.valueOf(lon));
-                    }
-                } catch (Exception e) {
-                    Toast.makeText(GetCityAddress.this,"Address is not valid",Toast.LENGTH_LONG);
-                }
-            }
-        });
-    }
-}
+                EditText editText=(EditText)findViewById(R.id.addressET);
+                final String addressgiven = editText.getText().toString();
+                            Geocoder geocoder=new Geocoder(getBaseContext(), Locale.getDefault());
+                            //String result=null;
+                            try {
+                                List addressList = geocoder.getFromLocationName(addressgiven, 1);
+                                if (addressList != null && addressList.size() > 0) {
+                                    Address address = (Address) addressList.get(0);
+                                    lt=String.valueOf(address.getLatitude());
+                                    lg=String.valueOf(address.getLongitude());
+                                    latitu.setText(lt);
+                                    latLongTV.setText(lg);
+                                    geoFire.setLocation("firebase-hq", new GeoLocation(address.getLatitude(), address.getLongitude())
+                                            , new GeoFire.CompletionListener() {
+                                                @Override
+                                                public void onComplete(String key, DatabaseError error) {
+                                                    if (error != null) {
+                                                        Toast.makeText(GetCityAddress.this,"There was an error saving the location to GeoFire: ",Toast.LENGTH_LONG).show();
+                                                    } else {
+                                                        btnMap.setOnClickListener(new View.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(View v) {
+                                                                startActivity(new Intent(GetCityAddress.this,MapsActivity.class));
+                                                            }
+                                                        });
+                                                        Toast.makeText(GetCityAddress.this,"\"Location saved on server successfully!\"",Toast.LENGTH_LONG).show();
+                                                    }
+                                                }
+                                            });
+                                }
+                            } catch (IOException e) {
+                                Log.e(TAG, "Unable to connect to Geocoder", e);
+                            }
+                        }
+            });
+      }
+       }
--- app/src/main/java/com/example/gh/mapdemo/MapsActivity.java
+++ app/src/main/java/com/example/gh/mapdemo/MapsActivity.java
@@ -1,25 +1,22 @@
 package com.example.gh.mapdemo;
 
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.LocationCallback;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
-import java.util.List;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,7 +25,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     MarkerOptions markerOptions;
     LatLng latLng;
     TextView latitude,longitude;
-
+    double lat=0.0,log=0.0;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,135 +34,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-       /* if (googleServicesAvailable()) {
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        } else {
-
-        }
-    }
-
-   /* private boolean googleServicesAvailable() {
-        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
-        int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if (isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        } else if (api.isUserResolvableError(isAvailable)) {
-            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "Cannoy Connect To Play Services", Toast.LENGTH_LONG).show();
-        }
-        return false;
-    }*/
-
-    /*@Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "First enable LOCATION ACCESS in settings.", Toast.LENGTH_LONG).show();
-            return;
-        }else{
-            if(!mMap.isMyLocationEnabled()){
-                mMap.setMyLocationEnabled(true);
-                LocationManager lm=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-                Location mylocation=lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if(mylocation==null){
-                    Criteria criteria=new Criteria();
-                    criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-                    String provider=lm.getBestProvider(criteria,true);
-                    mylocation=lm.getLastKnownLocation(provider);
-                }
-                if (mylocation!=null){
-                    LatLng userLocation=new LatLng(mylocation.getLatitude(),mylocation.getLongitude());
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,mMap.getMaxZoomLevel()-5));
-                    mMap.addMarker(new MarkerOptions().position(userLocation).title("Umair Ali Don"));
-                    mMap.setIndoorEnabled(true);
-                    mMap.setMapType(googleMap.MAP_TYPE_NORMAL);
-                }
-            }*/
-        }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap=googleMap;
-        Button btn_find = (Button) findViewById(R.id.btn_find);
-        latitude=(TextView) findViewById(R.id.latitude);
-        longitude=(TextView)findViewById(R.id.longitude);
-
-        btn_find.setOnClickListener(new View.OnClickListener() {
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        GeoFire geoFire=new GeoFire(databaseReference);
+        geoFire.getLocation("firebase-hq", new LocationCallback() {
             @Override
-            public void onClick(View v) {
-                EditText etLocation = (EditText) findViewById(R.id.et_location);
+            public void onLocationResult(String key, GeoLocation location) {
+                if (location != null) {
+                    Toast.makeText(MapsActivity.this,"The location for key %s is [%f,%f]",Toast.LENGTH_LONG).show();
+                   // System.out.println(String.format("The location for key %s is [%f,%f]", key, location.latitude, location.longitude));
+                    lat=location.latitude;
+                    log=location.longitude;
 
-                // Getting user input location
-                String location = etLocation.getText().toString();
-
-                if(location!=null && !location.equals("")){
-                    new GeocoderTask().execute(location);
+                } else {
+                    Toast.makeText(MapsActivity.this,"There is no location for key %s in GeoFire",Toast.LENGTH_LONG).show();
+                   // System.out.println(String.format("There is no location for key %s in GeoFire", key));
                 }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MapsActivity.this,"There was an error getting the GeoFire location:",Toast.LENGTH_LONG).show();
+                //System.err.println("There was an error getting the GeoFire location: " + databaseError);
             }
         });
     }
-
-    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
-        @Override
-        protected List<Address> doInBackground(String... locationName) {
-            // Creating an instance of Geocoder class
-            Geocoder geocoder = new Geocoder(getBaseContext());
-            List<Address> addresses = null;
-
-            try {
-                // Getting a maximum of 3 Address that matches the input text
-                addresses = geocoder.getFromLocationName(locationName[0], 3);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return addresses;
-        }
-
-        @Override
-        protected void onPostExecute(List<Address> addresses) {
-            if(addresses==null || addresses.size()==0){
-                Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
-            }
-
-            // Clears all the existing markers on the map
-            mMap.clear();
-
-            // Adding Markers on Google Map for each matching address
-            for(int i=0;i<addresses.size();i++){
-
-                Address address = (Address) addresses.get(i);
-
-                // Creating an instance of GeoPoint, to display in Google Map
-                latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                int lat= (int) address.getLatitude();
-                int lon= (int) address.getLongitude();
-                latitude.setText(String.valueOf(lat));
-                longitude.setText(String.valueOf(lon));
-//                Toast.makeText(MapsActivity.this,lat,Toast.LENGTH_LONG).show();
-             //   Toast.makeText(MapsActivity.this,lon,Toast.LENGTH_LONG).show();
-                String addressText = String.format("%s, %s",
-                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
-                        address.getCountryName());
-
-                markerOptions = new MarkerOptions();
-                markerOptions.position(latLng);
-                markerOptions.title(addressText);
-
-                mMap.addMarker(markerOptions);
-
-                // Locate the first location
-                if(i==0)
-                    mMap.getUiSettings().setZoomControlsEnabled(true);
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-            }
-        }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        LatLng sydney = new LatLng(lat,log);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 }
